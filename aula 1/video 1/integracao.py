8cb73549c7ce52137179c64a774e145716ac6ce1
ALMSantana <andrelms4@insper.edu.br>
2023-11-30 10:05:11 -0300

    Começando o curso 2

---
+++ aula 1/video 1/integracao.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template, request, Response
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+modelo = "gpt-4"
+
+app = Flask(__name__)
+app.secret_key = 'alura'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+#Novo Video 1
+@app.route("/chat", methods=["POST"])
+def chat():
+    prompt = request.json["msg"]
+    resposta = bot(prompt = prompt)
+
+if __name__ == "__main__":
+    app.run(debug = True)
